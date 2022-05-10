a1cae959000bc919c141984e3be6d63118308ab9
Colleen Murray <colleenkhm@gmail.com>
2022-05-10 17:18:22 -0500

    sleep model

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+--drop database 
+DROP DATABASE IF EXISTS dreams_db;
+--create database
+CREATE DATABASE dreams_db;
+
+-- USE dreams_db;
---
+++ models/Sleep.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Sleep extends Model {}
+
+Sleep.init(
+    {
+       id: {
+           type: DataTypes.INTEGER,
+           allowNull: false,
+           primaryKey: true,
+           autoIncrement: true
+       },
+       title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [30]
+            }
+       },
+       sleep_description: {
+           type: DataTypes.STRING,
+           allowNull: true
+       },
+       hours_slept: {
+           type: DataTypes.DECIMAL,
+           allowNull: true
+       },
+       dream_sw: {
+           type: DataTypes.BOOLEAN,
+           allowNull: false
+       },
+       dream_description: {
+           type: DataTypes.STRING,
+           allowNull: true
+       },
+       user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+       },
+       tag_id: {
+           type: DataTypes.INTEGER,
+           references: {
+               model: 'tag',
+               key: 'id'
+           }
+       }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'sleep',
+    }
+)
+
+module.exports = 'sleep'
