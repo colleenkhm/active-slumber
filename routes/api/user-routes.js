10df63f95240b0d9ae6291d07650b2590a82be11
Colleen Murray <colleenkhm@gmail.com>
2022-05-13 17:49:00 -0500

    created user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,97 @@
+const router = require('express').Router();
+const { User, Sleep, Tag } = require('../../models');
+
+// create GET route to get all users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create GET route to get one user
+router.get('/:id', async (req, res) => {
+    try{
+      const userData = await User.findByPk(req.params.id,{
+        include: [{model: Sleep}],
+        attributes: {
+            exclude: ['password']
+        }
+      });
+  
+      if(!userData){
+        res.status(404).json({message: 'No Category found with that id!'});
+        return;
+      }
+      
+      res.status(200).json(userData);
+    }catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// create POST route to create a user
+router.post('/', async (req, res) => {
+// create a new user
+try{
+    const userData = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+    });
+    req.session.save(() => {
+    req.session.loggedIn = true;
+    });
+    res.status(200).json(userData);
+}catch (err) {
+    res.status(400).json(err);
+}
+});
+
+// login
+
+router.post('/login', async (req, res) => {
+    try{
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (!userData) {
+            res.status(400).json({message: 'Incorrect email or password'});
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect email or password'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json({user: userData, message: 'You are now logged in'});
+        });
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// create POST route to allow user to log out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
