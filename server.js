03b503d5aec486658ed01e49bf22c9e8624c94de
CullerWhale <jcullenharris@gmail.com>
2022-05-14 11:24:41 -0500

    connect server

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then( () => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
+
 
