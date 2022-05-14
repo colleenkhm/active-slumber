9338a94c26a8a8579b8187dbbe0cb748cb265c33
CullerWhale <jcullenharris@gmail.com>
2022-05-14 11:16:42 -0500

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,43 @@ const { Tag, Sleep, SleepTag } = require('../../models');
 
 // create GET route to get all tags
 router.get('/', async (req, res) => {
-    try {
-        const tagData = await Tag.findAll();
-        res.status(200).json(tagData);
+    // find all tags
+    // be sure to include its associated Product data
+    try{
+      const tagData = await Tag.findAll({
+        include:
+        [{
+          model: Sleep, SleepTag,
+          attributes: ['title', 'sleep_description']
+        }]
+      });
+      res.status(200).json(tagData);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
-});
+  });
+
+  router.get('/:id', async (req, res) => {
+    // find a single tag by its `id`
+    // be sure to include its associated Sleep data
+    try{
+      const tagData = await Tag.findByPk(req.params.id,{
+        include:
+        [{
+          model: Sleep, SleepTag, 
+          attributes: ['title', 'sleep_description']
+        }]
+      });
+      if(!tagData){
+        res.status(404).json({message: 'No Tag found with that id!'});
+        return;
+      }
+      res.status(200).json(tagData);
+    }catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
 
 module.exports = router;
