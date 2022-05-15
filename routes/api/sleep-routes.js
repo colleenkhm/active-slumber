7253db25657c05537a99ced268249dab2a28809c
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-05-15 10:35:07 -0500

    Fix Sleep post/put routes

--- routes/api/sleep-routes.js
+++ routes/api/sleep-routes.js
@@ -47,52 +47,78 @@ router.get('/:id', async (req, res) => {
 });
 
 // create POST route to post sleep entry
-router.post('/', async (req, res) => {
-    // create a new sleep entry
-    try {
-        const sleepData = await Sleep.create({
-            title: req.body.title,
-            sleep_description: req.body.sleep_description,
-            hours_slept: req.body.hours_slept,
-            dream_sw: req.body.dream_sw,
-            dream_description: req.body.dream_description,
-            tagIds: req.body.tagIds,
-            user_id: req.body.user_id
-        });
-        if (req.body.tagIds.length) {
-            const sleepTagIdArr = req.body.tagIds.map((tag_id) => {
-              return {
-                sleep_id: Sleep.id,
+router.post('/', (req, res) => {
+    Sleep.create({
+        title: req.body.title,
+        sleep_description: req.body.sleep_description,
+        hours_slept: req.body.hours_slept,
+        dream_sw: req.body.dream_sw,
+        dream_description: req.body.dream_description,
+        user_id: req.body.user_id,
+        tagIds: req.body.tagIds 
+    })
+    .then((sleep) => {
+    // if there's sleep tags, we need to create pairings to bulk create in the SleepTag model
+    if (req.body.tagIds.length) {
+        const sleepTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+                sleep_id: sleep.id,
                 tag_id,
-              };
-            });
-            return SleepTag.bulkCreate(sleepTagIdArr);
-        }
-        res.status(200).json(sleepData);
-    } catch (err) {
-        res.status(400).json(err);
+            };
+        });
+        return SleepTag.bulkCreate(sleepTagIdArr);
     }
+    // if no sleep tags, just respond
+    res.status(200).json(sleep);
+    })
+    .then((sleepTagIds) => res.status(200).json(sleepTagIds))
+    .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+    });
 });
 
-router.put('/:id', async (req, res) => {
-    // update a sleep by its `id` value
-    try{
-      const sleepData = await Sleep.update(req.body, {
-          where: {
-            id: req.params.id
-          }
-        }
-      );
-  
-      if (!sleepData) {
-        res.status(404).json({ message: 'Sleep object found with that id!' });
-        return;
-      }
-  
-      res.status(200).json(sleepData);
-    }catch (err){
-      res.status(500).json(err);
-    }
+
+// Update Sleep
+router.put('/:id', (req, res) => {
+    // update product data
+    Sleep.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then(() => {
+    // find all associated tags from SleepTag
+    return SleepTag.findAll({ where: { sleep_id: req.params.id } });
+    })
+    .then((sleepTags) => {
+    // get list of current tag_ids
+    const sleepTagIds = sleepTags.map(({ tag_id }) => tag_id);
+    // create filtered list of new tag_ids
+    const newSleepTags = req.body.tagIds
+        .filter((tag_id) => !sleepTagIds.includes(tag_id))
+        .map((tag_id) => {
+            return {
+                sleep_id: req.params.id,
+                tag_id,
+            };
+        });
+    // figure out which ones to remove
+    const sleepTagsToRemove = sleepTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+    // run both actions
+        return Promise.all([
+            SleepTag.destroy({ where: { id: sleepTagsToRemove } }),
+            SleepTag.bulkCreate(newSleepTags),
+        ]);
+    })
+    .then((updatedSleepTags) => res.json(updatedSleepTags))
+    .catch((err) => {
+    // console.log(err);
+    res.status(400).json(err);
+    });
 });
 
 module.exports = router;
