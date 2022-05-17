e9f9219da49feb86660f1188080a63b4a956d55c
Colleen Murray <colleenkhm@gmail.com>
2022-05-17 15:12:03 -0500

    login debugging

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -24,4 +24,31 @@ async function loginForm(event) {
     }
 }
 
+async function signupForm(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginForm);
+document.querySelector('.signup-form').addEventListener('submit', signupForm);
