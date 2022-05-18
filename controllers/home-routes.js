96d21e8d0ec161423091c692301bce82f7fe900b
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-05-17 19:11:20 -0500

    Start Create Home Page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,39 @@
 const router = require('express').Router();
 const { Sleep, SleepTag, User, Tag } = require('../models');
 
-router.get('/', (req,res) => {
-    res.render('homepage');
-})
+router.get('/', async (req,res) => {
+    try {
+        const sleepData = await Sleep.findAll({
+            attributes: [
+                'title',
+                'sleep_description',
+                'hours_slept',
+                'dream_sw',
+                'dream_description'
+            ],
+            include: [
+                {
+                   model: User,
+                   attributes:['username'] 
+                },
+                {
+                    model: SleepTag,
+                    attributes:['tag_id'],
+                    include: {
+                        model: Tag,
+                        attributes:['tag_name']
+                    }
+                }
+            ]
+
+        });
+
+        const sleep = await sleepData.map(post => post.get({plain: true}));
+        res.render('homepage', {sleep});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
